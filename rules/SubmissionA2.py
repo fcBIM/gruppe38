4fac30f820514e7168e528da88ba9ca586c91605
KasperBIM <kasperter@hotmail.com>
2024-09-30 15:24:31 +0200

    Update SubmissionA2.py

--- rules/SubmissionA2.py
+++ rules/SubmissionA2.py
@@ -5,5 +5,4 @@
 
 #A2b: The chosen building from last years Advanced Building Design course is going to be building #2406.
 # -Claim: 6,74 kgC02eq/m2/year.
-#Claim is from the ARC report: BEATS -> CO2 -> 6,74 kgC02eq./m2/yr.
-#Source of claim: file CES_BLD_24_06_ARC p. 5. 
+#Claim is from the ARC report: BEATS -> CO2 -> 6,74 kgC02eq./m2/yr. Source of claim: file CES_BLD_24_06_ARC p. 5. 
