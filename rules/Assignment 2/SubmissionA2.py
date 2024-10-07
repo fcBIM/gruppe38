3b7f71e31eb8755dd0d9a383f349a56db88df4be
KasperBIM <kasperter@hotmail.com>
2024-10-07 16:10:33 +0200

    Delete rules/Assignment 2/SubmissionA2.py

--- rules/Assignment 2/SubmissionA2.py
+++
@@ -1,8 +0,0 @@
-#Formalities:
-#Group 38 - Sustainability and materials.
-
-#A2a: Disagree. The focus area is Sustainability and materials where the main objective is to be an analyst and export a (CSV) file with a total GWP score for specified IfcElements.
-
-#A2b: The chosen building from last years Advanced Building Design course is going to be building #2406.
-# -Claim: 6,74 kgC02eq/m2/year.
-#Claim is from the ARC report: BEATS -> CO2 -> 6,74 kgC02eq./m2/yr. Source of claim: file CES_BLD_24_06_ARC p. 5. 
