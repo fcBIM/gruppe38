466d45e39d0551360c9d25273fabd22d1f96c984
fcBIM <fredesemai@gmail.com>
2024-10-28 15:34:26 +0100

    Update main.py

--- rules/Assignment 3/main.py
+++ rules/Assignment 3/main.py
@@ -1,24 +1,50 @@
-#import ifcopenshell
-#Import the correct IFC model:
-#model = ifcopenshell.open('/Users/fredemollegaard/Desktop/Adv.BIM/CES_BLD_24_06_ARC.ifc')
-#model = ifcopenshell.open('/Users/fredemollegaard/Desktop/Adv.BIM/CES_BLD_24_06_STR.ifc')
 
-def roofArea(model):
-    #Making sure the value of property doesn't change if run multiple times
-    total_roof_area = 0
+import json
+import ifcopenshell
+import os
+import time
+import uuid
 
-    #First we define the element we are interested in. For this case the object type IFCRoof.
-    for entity in model.by_type("IfcRoof"):
+#IFC file import is taken from https://github.com/timmcginley/
+'''
+    load the IFC file
+'''
+name = '/Users/fredemollegaard/Desktop/Adv.BIM/CES_BLD_24_06_ARC'
+
+model_url = name+".ifc"
+start_time = time.time()
+
+if (os.path.exists(model_url)):
+    model = ifcopenshell.open(model_url)
+    print("\n\tFile    : {}.ifc".format(name))
+    print("\tLoad    : {:.2f}s".format(float(time.time() - start_time)))
+
+    #start_time = time.time()
+    #print("\tConvert : {:.4f}s".format(float(time.time() - start_time)))
+
+else:
+    print("\nERROR: please check your model folder : " +model_url+" does not exist")
+
+
+
+
+# Retrieve all wall types in the IFC file
+wall_types = model.by_type("IfcWallType")
+
+# If you want to get individual wall instances (e.g., IfcWall)
+walls = model.by_type("IfcWall")
+
+# Print out the names or IDs of all wall types
+print("Wall Types in IFC File:")
+for wall_type in wall_types:
+    print(f"Wall Type Name: {wall_type.Name}, ID: {wall_type.id()}")
+
+# Optional: Print information for individual wall instances
+print("\nIndividual Wall Instances in IFC File:")
+for wall in walls:
+    print(f"Wall Name: {wall.Name}, ID: {wall.id()}")
+
     
 
-        #Secondly we define the relation between the object type's different IfcElementQuantities and sort through to find the Area.
-        for relDefinesByProperties in entity.IsDefinedBy:
-            for prop in relDefinesByProperties.RelatingPropertyDefinition.HasProperties:
-                
-                if prop.Name == 'Area':
-                    #Thirdly we ask it to calculate the total area of all the specified object types - IfcRoof.
-                    total_roof_area += prop.NominalValue.wrappedValue
 
-    #Fourthly, and last, we print the desired value and add a short descriptive text, for the reader to have a clear indication of what has been printed.
-    result = print(f"\n The roof in the arc model has an area of {total_roof_area} square meters")
-    return result
+
