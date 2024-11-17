bc171dbc5dd0993df44ebd4e0dd24e4f675d3686
KasperBIM <kasperter@hotmail.com>
2024-11-17 17:02:11 +0100

    Update excelimport.py

--- rules/Assignment 3/excelimport.py
+++ rules/Assignment 3/excelimport.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
-import json as json
+import json
 
 
 # Load the Excel file
-file_path_xl = "C:\\Users\\kaspe\\OneDrive\\Skrivebord\\DTU\\3. semester\\41934 - Advanced BIM\\Excel_EPD_Data.xlsx" #Insert the correct file path 
-df = pd.read_excel(file_path_xl)
+file_path_csv = "Load path"
+df = pd.read_csv(file_path_csv, sep=';', encoding='latin1')
 
-# Convert the DataFrame into a matrix (NumPy array) and then to a list
+# Convert the DataFrame into a matrix and then to a list
 matrix = df.to_numpy().tolist()
 
 # Convert the list to a JSON string
 json_data = json.dumps(matrix, indent=4)
 
 # Save the JSON string to a file
-output_path = r"C:\Users\kaspe\OneDrive\Skrivebord\DTU\3. semester\41934 - Advanced BIM\Excel_EPD_Data.json"
+output_path = r"Export JSON string to file path."
 with open(output_path, 'w') as json_file:
     json_file.write(json_data)
 
